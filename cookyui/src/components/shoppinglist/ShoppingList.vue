c96759c4bd4688d635a8a9a939892e5eee81e439
Sascha <sascha.hartleb@googlemail.com>
2024-05-06 22:31:43 +0200

    added an option to insert shopping items anywhere in the list adding a new shopping item now adds the item as an empty item directly in the list. The add popup was removed Buttons of the shopping list are now sticky Enter will make the currently edited shopping item read only and automatically add a new shopping item

--- cookyui/src/components/shoppinglist/ShoppingList.vue
+++ cookyui/src/components/shoppinglist/ShoppingList.vue
@@ -1,10 +1,5 @@
 <template>
     <div>
-        <div class="row mt-1">
-            <div class="col-12 text-right">
-                <button type="button" class="btn btn-secondary m-1" @click="addNewItem()">Hinzufügen</button>
-            </div>
-        </div>
         <div class="row">
             <div class="col">
                 We added the ingredients of the following recipes:
@@ -15,6 +10,12 @@
                 <li v-for="(recipeName, key) in recipeNames" :key="key" >{{ recipeName }}</li>
             </ul>
         </div>
+        <div class="row m-1 stickybuttons">
+            <button type="button" class="btn btn-secondary m-1" @click="clearList()">Leeren</button>
+            <button type="button" class="btn btn-secondary m-1" @click="saveList()">Speichern</button>
+            <button type="button" class="btn btn-secondary m-1" @click="copyList()">Kopieren</button>
+            <button type="button" class="btn btn-secondary m-1" @click="addNewItem()">Hinzufügen</button>
+        </div>
         <table class="table table-hover">
             <thead style="background-color:#AFBC6C">
                 <th class="py-2">Name</th>
@@ -23,70 +24,36 @@
                 <th width="110px" />
             </thead>
             <tbody>
-                <tr v-for="(item,key) in items" :key="key" draggable="true"
+                <tr v-for="(item,key) in items" :key="key" draggable="true" @mousemove="blorp"
                     @drop="finishDrag" @dragenter="changeOrder(item)" @dragstart="startDrag(item, key)">
                     <td class="p-2" ref="shoppingItemName" @click="changeEditMode(item, key, 'Name')">
-                        <input v-if="item.isEditable" v-model="item.name" type="text" style="width: 100%;">
+                        <input v-if="item.isEditable" v-model="item.name" type="text" style="width: 100%;" @keypress="onEnterKey(item, $event)">
                         <div v-else>{{item.name}}</div>
                     </td>
                     <td class="p-2" ref="shoppingItemAmount" @click="changeEditMode(item, key, 'Amount')">
-                        <input v-if="item.isEditable" v-model="item.amount" type="number" style="width: 100%;">
+                        <input v-if="item.isEditable" v-model="item.amount" type="number" style="width: 100%;" @keypress="onEnterKey(item, $event)">
                         <div v-else>{{item.amount}}</div>
                     </td>
                     <td class="p-2" ref="shoppingItemUnit" @click="changeEditMode(item, key, 'Unit')">
-                        <input v-if="item.isEditable" v-model="item.unit" type="text" style="width: 100%;">
+                        <input v-if="item.isEditable" v-model="item.unit" type="text" style="width: 100%;" @keypress="onEnterKey(item, $event)">
                         <div v-else>{{item.unit}}</div>
                     </td>
                     <td class="p-2 " >
-                        <button v-if="item.isEditable" type="button" class="btn btn-secondary fa fa-check" @click="removeEditMode(item)" />
+                        <button v-if="item.isEditable" type="button" class="btn btn-secondary fa fa-check" @click="removeEditMode(item, key)" />
                         <button type="button" class="btn btn-secondary fa fa-trash" @click="removeItem(item, key)" />
                     </td>
                 </tr>
             </tbody>
         </table>
-        <div class="row">
-            <div class="col-12 text-right">
-                <button type="button" class="btn btn-secondary m-1" @click="clearList()">Alles neu</button>
-                <button type="button" class="btn btn-secondary m-1" @click="saveList()">Speichern</button>
-                <button type="button" class="btn btn-secondary" @click="copyList()">Kopieren</button>
-            </div>
-        </div>
-
         <textarea id="copyField" hidden="true" />
 
-        <!-- shopping item edit popup -->
-        <baseModal ref="modalItemEdit">
-            <template #header>
-                Neuer Eintrag
-            </template>
-
-            <template #body>
-                <label class="m-1">Name</label>
-                <input v-model="itemEdit.name" type="text" class="m-1">
-
-                <label class="m-1">Menge</label>
-                <input v-model="itemEdit.amount" type="number" class="m-1">
-
-                <label class="m-1">Einheit</label>
-                <input v-model="itemEdit.unit" type="text" class="m-1">
-            </template>
-
-            <template #footer>
-                <button type="button" class="col-3 btn btn-outline-primary" @click="cancelModalItemEdit()">cancel</button>
-                <button type="button" class="col-3 btn btn-outline-primary" @click="closeModalItemEdit()">okay</button>
-            </template>
-        </baseModal>
+        <div class="hugo" ref="hugo" />
     </div>
 </template>
 
 <script>
-
-import baseModal from '.././Modal.vue'
-
 export default {
     name: 'RecipeList',
-
-    components: { baseModal },
 
     data () {
         return {
@@ -96,6 +63,7 @@ export default {
             // the item that is currently edited in the edit-popup
             itemEdit: null,
             previousEditedItem: null,
+            addItemEventActive: false,
         }
     },
 
@@ -105,22 +73,65 @@ export default {
     },
 
     methods: {
-        removeEditMode (shoppingItem) {
+        blorp (event) {
+            var rect = event.currentTarget.getBoundingClientRect()
+            const hugosStyle = this.$refs.hugo.style
+
+            let addAreaReached = false
+
+            if (event.clientY - rect.top < 5) {
+                hugosStyle.top = (rect.top - 8) + 'px'
+                addAreaReached = 0
+            } else if (event.clientY > rect.bottom - 5) {
+                hugosStyle.top = (rect.bottom - 8) + 'px'
+                addAreaReached = 1
+            }
+
+            if (addAreaReached !== false) {
+                hugosStyle.left = rect.x + 'px'
+                hugosStyle.width = rect.width + 'px'
+                if (hugosStyle.animation !== 'fadeIn 0.3s') {
+                    hugosStyle.animation = 'fadeIn 0.3s'
+                    hugosStyle.opacity = '100%'
+                    this.addItemEventActive = addAreaReached
+                }
+            } else {
+                if (hugosStyle.animation !== 'fadeOut 0.3s') {
+                    hugosStyle.animation = 'fadeOut 0.3s'
+                    hugosStyle.opacity = '0%'
+                    this.addItemEventActive = false
+                }
+            }
+        },
+        removeEditMode (shoppingItem, key) {
+            if (shoppingItem.id === undefined && !shoppingItem.name) {
+                this.items.splice(key, 1)
+            }
             shoppingItem.isEditable = false
         },
-
         changeEditMode (shoppingItem, key, refKey) {
-            if (this.previousEditedItem) {
-                this.previousEditedItem.isEditable = false
+            if (this.addItemEventActive !== false) {
+                key += this.addItemEventActive
+                this.items.splice(key, 0, { isEditable: true })
+                refKey = 'Name'
+            } else {
+                if (this.previousEditedItem) {
+                    this.previousEditedItem.isEditable = false
+                }
+                shoppingItem.isEditable = true
+                this.previousEditedItem = shoppingItem
             }
-            shoppingItem.isEditable = true
-            this.previousEditedItem = shoppingItem
 
             this.$nextTick(() => {
                 this.$refs['shoppingItem' + refKey][key].getElementsByTagName('input')[0].focus()
             })
         },
-
+        onEnterKey (shoppingItem, event) {
+            if (event.key === 'Enter') {
+                shoppingItem.isEditable = false
+                this.addNewItem()
+            }
+        },
         /*
          * copy the item list to the clipboard by creating a string representation and stuff that one into an hidden text area.
          * Then call the copy function from there and hope that it works
@@ -142,7 +153,6 @@ export default {
             })
 
             const copyField = document.querySelector('#copyField')
-
             copyField.hidden = false
             copyField.value = itemText
 
@@ -160,7 +170,6 @@ export default {
             copyField.hidden = true
             window.getSelection().removeAllRanges()
         },
-
         async clearList () {
             await window.cookyFetch('/rest/shoppinglist/', 'DELETE')
             this.items = []
@@ -171,22 +180,12 @@ export default {
                 name: null,
                 amount: null,
                 unit: null,
+                isEditable: true,
             }
-
-            this.$refs.modalItemEdit.show()
-        },
-        cancelModalItemEdit () {
-            this.$refs.modalItemEdit.close()
-        },
-        closeModalItemEdit () {
-            this.$refs.modalItemEdit.close()
-
-            const foundItems = this.items.filter(item => item.name === this.itemEdit.name)
-            if (foundItems.length > 0) {
-                foundItems[0].amount += this.itemEdit.amount
-            } else {
-                this.items.push(this.itemEdit)
-            }
+            const arrayLength = this.items.push(this.itemEdit)
+            this.$nextTick(() => {
+                this.$refs.shoppingItemName[arrayLength - 1].getElementsByTagName('input')[0].focus()
+            })
         },
         async removeItem (item, key) {
             if (item.id) {
@@ -240,3 +239,30 @@ export default {
     },
 }
 </script>
+
+<style>
+.hugo {
+    position: fixed;
+    height: 16px;
+    background-color: rgb(175, 188, 108);
+    top: 10px;
+    left: 10px;
+    pointer-events: none;
+}
+.stickybuttons {
+    position: sticky;
+    background-color: rgba(175, 188, 108, 0.5);
+    top: 0;
+    float: right
+}
+
+@keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@keyframes fadeOut {
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+}
+</style>
